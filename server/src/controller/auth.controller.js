49906cf7c71d327ca6ad861af8c37ff9a4254c9c
blackeye75 <priyanshuraj22275@gmail.com>
2025-06-20 19:39:10 +0530

    Add JWT authentication to user signup; include avatar generation and cookie handling

--- server/src/controller/auth.controller.js
+++ server/src/controller/auth.controller.js
@@ -1,36 +1,53 @@
 import User from '../models/User.model.js';
+import jwt from 'jsonwebtoken';
 
 export async function signup(req, res) {
-    const { email, password, fullName } = req.body;
+  const { email, password, fullName } = req.body;
 
-    try {
-        if (!email || !password || !fullName) {
-            return res.status(400).json({ message: " all the fields are required!" });
-        }
-        if (password.length < 6) {
-            return res.status(400).json({ message: "Password must be at least 6 characters long!" });
-        }
-
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-        if (!emailRegex.test(email)) {
-            return res.status(400).json({ message: "Invalid email format" });
-        }
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-            return res.status(400).json({ message: "User already exists with this email!" });
-        }
-
-        const idx = Math.floor(Math.random() * 100) + 1; // generate a num between 1-100
-        const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`;
-
-    } catch (error) {
-
+  try {
+    if (!email || !password || !fullName) {
+      return res.status(400).json({ message: " all the fields are required!" });
     }
+    if (password.length < 6) {
+      return res.status(400).json({ message: "Password must be at least 6 characters long!" });
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "Invalid email format" });
+    }
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists with this email!" });
+    }
+
+    const idx = Math.floor(Math.random() * 100) + 1; // generate a num between 1-100
+    const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`;
+    const newUser = await User.create({
+      email,
+      password,
+      fullName,
+      profilePic: randomAvatar
+    });
+
+    const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, { expiresIn: '30d' });
+
+    res.cookie('jwt', token, {
+      maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days in milliseconds
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
+      sameSite: 'strict', // Helps prevent CSRF attacks
+    });
+
+    return res.status(201).json({ message: "User created successfully!", success: true, user: newUser });
+  } catch (error) {
+
+  }
 }
 export function login(req, res) {
-    res.send('Hello from auth routes!')
+  res.send('Hello from auth routes!')
 }
 export function logout(req, res) {
-    res.send('Hello from auth routes!')
+  res.send('Hello from auth routes!')
 }
