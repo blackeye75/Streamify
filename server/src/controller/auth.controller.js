0b4fdbd1634c6c23784538ab57b35ebbf231d33e
blackeye75 <priyanshuraj22275@gmail.com>
2025-06-20 13:38:54 +0530

    Implement user signup functionality with validation and password hashing; add MongoDB connection and user model

--- server/src/controller/auth.controller.js
+++ server/src/controller/auth.controller.js
@@ -1,9 +1,36 @@
-export function signup (req, res) {
+import User from '../models/User.model.js';
+
+export async function signup(req, res) {
+    const { email, password, fullName } = req.body;
+
+    try {
+        if (!email || !password || !fullName) {
+            return res.status(400).json({ message: " all the fields are required!" });
+        }
+        if (password.length < 6) {
+            return res.status(400).json({ message: "Password must be at least 6 characters long!" });
+        }
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({ message: "Invalid email format" });
+        }
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: "User already exists with this email!" });
+        }
+
+        const idx = Math.floor(Math.random() * 100) + 1; // generate a num between 1-100
+        const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`;
+
+    } catch (error) {
+
+    }
+}
+export function login(req, res) {
     res.send('Hello from auth routes!')
 }
-export function login (req, res) {
-    res.send('Hello from auth routes!')
-}
-export function logout (req, res) {
+export function logout(req, res) {
     res.send('Hello from auth routes!')
 }
--- server/src/server.js
+++ server/src/server.js
@@ -3,12 +3,15 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import authRoutes from './routes/auth.routes.js';
+import { connectDB } from './lib/db.js';
 
 const app = express()
 const PORT= process.env.PORT || 5001;
 
+app.use(express.json());
 app.use("/api/auth",authRoutes)
 
 app.listen(5001,()=>{
     console.log(`Server is running on port ${PORT}`)
+    connectDB();
 })
