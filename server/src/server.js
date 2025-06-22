8a8ae4d378e2ece5262f81c5e2e322128122e796
blackeye75 <priyanshuraj22275@gmail.com>
2025-06-23 05:10:23 +0530

    Implement friend request functionality and chat routes; add FriendRequest model and update user routes

--- server/src/server.js
+++ server/src/server.js
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 
 import authRoutes from './routes/auth.routes.js';
 import userRoutes from './routes/user.routes.js';
+import chatRoutes from './routes/chat.routes.js';
 import { connectDB } from './lib/db.js';
 
 const app = express()
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth",authRoutes)
 app.use("/api/users",userRoutes)
+app.use("/api/chat",chatRoutes)
 
 app.listen(5001,()=>{
     console.log(`Server is running on port ${PORT}`)
