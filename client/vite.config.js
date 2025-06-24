24204f77b598cb4552d76bf64b2dab86a83dc538
blackeye75 <priyanshuraj22275@gmail.com>
2025-06-24 20:40:29 +0530

    feat: integrate Tailwind CSS and DaisyUI, add routing and new pages
    - Added Tailwind CSS and DaisyUI dependencies to the project.
    - Removed existing CSS styles and replaced them with Tailwind CSS imports.
    - Implemented React Router for navigation between different pages.
    - Created new pages: HomePage, SignUpPage, LoginPage, NotificationsPage, CallPage, ChatPage, and OnBoardingPage.
    - Integrated react-hot-toast for notifications.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import daisyui from 'daisyui'
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss(),tailwindcss({
+      config: {
+        content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+        // plugins: [daisyui],
+        daisyui: {
+          themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine"], // Add your themes here
+          darkTheme: "dark", // name of one of the included themes for dark mode
+        },
+      },
+    }),],
 })
